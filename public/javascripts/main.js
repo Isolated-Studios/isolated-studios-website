4994437a7d1d164c278d98d1b257407927d5308d
real-gravity <hfrancome@yahoo.com.au>
2023-03-31 19:48:41 +0800

    Add files via upload

---
+++ public/javascripts/main.js
@@ -0,0 +1,21 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+document.querySelector("h1").onmouseenter = event => {
+      let iterations = 0;
+
+      const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+                  .map((letter, index) => {
+                        if (index < iterations) {
+                              return event.target.dataset.value[index];
+                        }
+
+                        return letters[Math.floor(Math.random() * 26)]
+                  })
+                  .join("");
+
+            if (iterations >= 16) clearInterval(interval);
+
+            iterations += 1 /3;
+      }, 30);
+}
